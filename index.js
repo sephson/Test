2a3a122877f06d8f79fa57f6f27ecd3a113fc66c
sephson <disutj@gmail.com>
2021-08-18 12:00:07 +0100

    working

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("HELOO");
+app.post("/", (req, res) => {
+  console.log("Data", req.body);
+  const { subject, email, text } = req.body;
+  if (!email || !text) {
+    res
+      .status(403)
+      .json({ message: "Please input your email and Write a Text" });
+  }
+  res.status(200).json({
+    message: "Your Message was Sent! Disu Will get in touch with you soon",
+  });
 });
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.listen(process.env.PORT || 5000, () => console.log("Server is running"));
+
+process.on("unhandledRejection", (err) => {
+  console.log(`Logged Error: ${err}`);
+  server.close(() => process.exit(1));
+});
